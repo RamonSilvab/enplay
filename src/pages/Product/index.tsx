6dc0ae014066a733cad5fc9c3c28f771c05f488b
RamonSilvaB <ramonsilvab05@gmail.com>
2024-08-15 17:09:20 -0300

    colocando o path para as rotas por id

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,3 +1,11 @@
-const Product = () => <div>produto</div>
+import { useParams } from 'react-router-dom'
+
+const Product = () => {
+  const teste = useParams()
+
+  console.log(teste)
+
+  return <div>produto</div>
+}
 
 export default Product
