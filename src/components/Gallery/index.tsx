e3fc2bbc7d976d1e6e5f7d71d38375da39dca293
RamonSilvaB <ramonsilvab05@gmail.com>
2024-08-29 19:56:54 -0300

    criando os modais

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,24 +1,64 @@
 import Section from '../Section'
-import hogwartsImg from '../../assets/images/hogwartsLegacy.png'
-import { Item, Items } from './styles'
 
-const Gallery = () => (
-  <Section title="Galeria" background="black">
-    <Items>
-      <Item>
-        <img src={hogwartsImg} alt="Hogwarts Legacy" />
-      </Item>
-      <Item>
-        <img src={hogwartsImg} alt="Hogwarts Legacy" />
-      </Item>
-      <Item>
-        <img src={hogwartsImg} alt="Hogwarts Legacy" />
-      </Item>
-      <Item>
-        <img src={hogwartsImg} alt="Hogwarts Legacy" />
-      </Item>
-    </Items>
-  </Section>
-)
+import { Item, Items, Action } from './styles'
+
+import hogwartsImg from '../../assets/images/hogwartsLegacy.png'
+import modal from '../../assets/images/modal.png'
+import spideman from '../../assets/images/banner-homem-aranha.png'
+import play from '../../assets/images/botao-play.png'
+import zoom from '../../assets/images/mais-zoom.png'
+
+type GalleryItem = {
+  type: 'image' | 'video'
+  url: string
+}
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: hogwartsImg
+  },
+  {
+    type: 'image',
+    url: modal
+  },
+  {
+    type: 'image',
+    url: spideman
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/78CP8na1Fpo?si=AVzjDe4SRREExjkt'
+  }
+]
+
+type Props = {
+  defaultCover: string
+}
+
+const Gallery = ({ defaultCover }: Props) => {
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  return (
+    <Section title="Galeria" background="black">
+      <Items>
+        {mock.map((media, index) => (
+          <Item key={media.url}>
+            <img
+              src={getMediaCover(media)}
+              alt={`Mídia de ${index + 1} Nome do Jogo`}
+            />
+            <Action>
+              <img src={zoom} alt="Clique para maximizar a imagem" />
+            </Action>
+          </Item>
+        ))}
+      </Items>
+    </Section>
+  )
+}
 
 export default Gallery
